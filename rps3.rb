e6c1ea448da073f1fed6625993cb662d27deef7d
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-01-26 09:45:43 -0600

    added 3rd rps challenge

---
+++ rps3.rb
@@ -0,0 +1,34 @@
+#Create a variable counter for wins, set to zero
+#Create a variable counter for loses, set to zero
+
+
+#Iterate Three times do 
+	#Display, chose Rock, Paper or Scissors
+	#Get user input, set to variable choice
+
+	#Get random selection from an array, set to variable comp
+
+	#Start an if statement: if both choices are the same
+		#Puts draw
+	#else if computer has rock and human has paper
+		#add a point for win
+	#else if computer has rock and human has scissors
+		#add a point for loses
+	#else if computer has scissors and human has rock
+		#add a point for win
+	#else if computer has scissors and human has paper
+		#add a point for loses
+	#else if computer has paper and human has rock
+		#add a point for loses
+	#else if computer has paper and human has scissors
+		#add a point for win
+
+
+#If wins > loses
+	#Display You win
+#Else
+	#Display You Lose
+
+
+#### BONUS #### 
+	#Display History of Moves at end
