fbd73a474d587084aed9c13e14430c1100f4f231
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-01-26 09:43:59 -0600

    mostly created pseudo exercises

---
+++ rps1.rb
@@ -0,0 +1,49 @@
+	
+#Declare a variable called best_of, set it equal to 3
+#Create a counter for player's wins, set it equal to 0
+#Create a counter for computer's wins, set it equal to 0
+
+
+#While computer_score and person score are less than 3
+
+	#puts Select Rock, Paper or Scissors
+	#get person's choice, set to variable persons_choice
+
+	#get computers choice randomly from an array, set to variable computers_choice
+
+	#Start an if statement: if both choices are the same
+		#Puts draw
+	#else if computer has rock and human has paper
+		#Puts You Win
+		#add a point for human
+	#else if computer has rock and human has scissors
+		#Puts Computer Wins
+		#add a point for computer
+	#else if computer has scissors and human has rock
+		#Puts You Win
+		#add a point for human
+	#else if computer has scissors and human has paper
+		#Puts Computer Wins
+		#add a point for computer
+	#else if computer has paper and human has rock
+		#Puts Computer Wins
+		#add a point for computer
+	#else if computer has paper and human has scissors
+		#Puts You Win
+		#add a point for human
+	#else
+		#puts enter a valid option and give options
+
+
+
+#If computer score == 2
+	#Puts Computer is the best of 3!
+#Else
+	#Puts Human is best of 3!
+	
+
+
+##BONUS##
+	#If time permits
+		#Create a way to show history of choices when declaring winner of 3
+
---
+++ rps2.rb
@@ -0,0 +1,26 @@
+
+#Set a variable for computer_win equal to 0
+#Set a variable for human_win equal to 0
+
+
+#Display “Welcome to RPS 2016. The winner gets rich, the loser dies.”
+#Display “Rock...Paper...Scissors...Shoot!”
+
+#Display “Press 1 for rock, 2 for paper, 3 for scissors”
+#Get user's choice, set to variable
+
+#Get computer's choice, select 1,2 or 3 randomly, set to a variable
+
+
+#Set Human_Win = 0
+#Set Comp_Win = 0
+
+#Repeat until Human_Win or Comp_Win = 2
+    #if user - comp = 1 OR user - comp = -2 
+    	#display “You win!” add 1 to Human_win
+    # else
+    	#display "You Lose!" add 1 to Computer_win
+
+
+#Display best of 3 winner
+
