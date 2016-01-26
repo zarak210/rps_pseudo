05b66f5273981faca567ca2b02c7099eefb4560c
Zara Khan <zarak210@gmail.com>
2016-01-26 13:29:28 -0600

    working on line 38

--- rps3.rb
+++ rps3.rb
@@ -1,15 +1,43 @@
+wins = 0
+loses = 0
+
+
+
+
 #Create a variable counter for wins, set to zero
 #Create a variable counter for loses, set to zero
 
 
 #Iterate Three times do 
 	#Display, chose Rock, Paper or Scissors
+	puts "chose-rock, paper, or scissors?"
+	chose = gets. chomp
 	#Get user input, set to variable choice
 
 	#Get random selection from an array, set to variable comp
 
 	#Start an if statement: if both choices are the same
-		#Puts draw
+if both choices the same 
+then puts draw
+elsif computer has rock and human has paper		
+	add point for win
+elsif computer has rock and human has scissors
+	add point for loses
+elsif computer has scissors and human has rock 
+	add point for win
+elsif computer has scissors and human has paper
+	add point for loses
+elsif computer has paper and human has rock 
+	add point for loses
+elsif computer has paper and human has scissors
+	add point for win
+if wins > loses
+		display "You Win!"
+if wins < loses 
+	display "You Lose!"
+end
+
+#Puts draw
 	#else if computer has rock and human has paper
 		#add a point for win
 	#else if computer has rock and human has scissors
