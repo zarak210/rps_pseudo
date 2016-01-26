0c2d0af85453fcaf8ef4a366925c0b13c1091d2c
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-01-26 10:10:30 -0600

    rps4 created

---
+++ rps4.rb
@@ -0,0 +1,24 @@
+#set win variable to zero
+#set loses variable to zero
+
+#Prompt user to chose r, p or s
+#get user input, set to choice variable, make sure it's lowercase
+
+#Select computer choice randomly from arrary, set to variable computer
+
+#Three time do iterate
+  #use case statement with [player, computer] (This will be challenging, but totally worth it!)
+    #when ['p', 'r'], ect..
+      #puts you win
+      # add one to win variable
+    #When ['r','r'], ect..
+      #Puts you tied
+    #Else
+      #You Lose
+      # add one to win variable
+
+#Declare Winner at end with score of wins and loses
+
+#### BONUS ####
+  #Show history of computer and user moves at end
+
