196e655215b8de6b73ae8a2570fe2037d7aeeb3a
zarak210 <zkhan@lsoc.org>
2016-01-27 11:38:54 -0600

    Update rps3.rb

--- rps3.rb
+++ rps3.rb
@@ -1,28 +1,15 @@
 wins = 0
 loses = 0
 
-
-
-
-#Create a variable counter for wins, set to zero
-#Create a variable counter for loses, set to zero
-
-
-#Iterate Three times do 
 	options = ['rock', 'paper', 'scissors']
 		puts options 
 		puts options [0]
 		puts options [1]
 		puts options [2]
 		
-	#Display, chose Rock, Paper or Scissors
 	puts "chose-rock, paper, or scissors?"
 	chose = gets. chomp
-	#Get user input, set to variable choice
 
-	#Get random selection from an array, set to variable comp
-
-	#Start an if statement: if both choices are the same
 if both choices the same 
 then puts draw
 elsif computer has rock and human has paper		
@@ -41,25 +28,4 @@ if wins > loses
 		display "You Win!"
 if wins < loses 
 	display "You Lose!"
-end
-
-#Puts draw
-	#else if computer has rock and human has paper
-		#add a point for win
-	#else if computer has rock and human has scissors
-		#add a point for loses
-	#else if computer has scissors and human has rock
-		#add a point for win
-	#else if computer has scissors and human has paper
-		#add a point for loses
-	#else if computer has paper and human has rock
-		#add a point for loses
-	#else if computer has paper and human has scissors
-		#add a point for win
-
-
-#If wins > loses
-	#Display You win
-#Else
-	#Display You Lose
 end
