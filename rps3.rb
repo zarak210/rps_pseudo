98ce0d8b1f52733e214b0020c95406de11e60d10
zarak210 <zkhan@lsoc.org>
2016-01-28 10:28:41 -0600

    Update rps3.rb

--- rps3.rb
+++ rps3.rb
@@ -4,29 +4,25 @@ loses = 0
 	3.times do
 	options = ['rock', 'paper', 'scissors'] 
 	
-		puts options 
-		puts options [0]
-		puts options [1]
-		puts options [2]
-		
-	puts "chose-rock, paper, or scissors?"
+	puts = "Chose - rock, paper, or scissors?"
 	chose = gets. chomp
 
-if both choices the same 
-then puts draw
-elsif computer has rock and human has paper		
-	then, win = +1
-elsif computer has rock and human has scissors
-	then loss = +1 
-elsif computer has scissors and human has rock 
+	if options == chose
+	then puts "draw"
+	elsif options == "rock" && chose == "paper"		
 	then win = +1
-elsif computer has scissors and human has paper
+	elsif options == rock && chose == "scissors"
+	then loses = +1 
+	elsif options = "scissors" && chose == "rock" 
+	then win = +1
+	elsif options = "scissors" && chose == "paper"
 	then, loss = +1
-elsif computer has paper and human has rock 
+	elsif options == "paper" && chose == "rock" 
 	then, win = +1 
-elsif computer has paper and human has scissors
+	elsif options == "paper" && chose == "scissors"
 	then, win = +1 
-end 
+	end
+ 
 end 
 
 if wins > loses
